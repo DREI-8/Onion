362c915564565baec1664e8e2dc8b004e5ce9f17
théophile <113911992+DREI-8@users.noreply.github.com>
2025-03-29 17:45:13 -0400

    feat: New header for to_device methods and addition capabilities

--- onion/cuda.h
+++ onion/cuda.h
@@ -4,5 +4,9 @@
 #include "tensor.h"
 
 bool is_cuda_available();
+void cpu_to_cuda(Tensor* tensor);
+void cuda_to_cpu(Tensor* tensor);
+void to_device(Tensor* tensor, const char* target_device);
+Tensor add_tensor_cuda(const Tensor& a, const Tensor& b);
 
 #endif // CUDA_H
